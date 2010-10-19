6dd0a7d704423eb7a2b6e7d2e9a63e3c66522184
Scott Turnbull <sturnbu@emory.edu>
2010-10-19 18:34:05 +0000

    Created basic tests so far and created django wrapper.

---
+++ pidservices/djangowrapper/__init__.py
@@ -0,0 +1,10 @@
+"""
+*"It turns out that an eerie type of chaos can lurk just behind a facade of
+order - and yet, deep inside the chaos lurks an even eerier type of order"*
+
+- **Douglas Hostadler**
+
+Provides a minimal Django wrapper for various pidman services to load
+various Django settings.
+
+"""
---
+++ pidservices/djangowrapper/shortcuts.py
@@ -0,0 +1,37 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+from django.conf import settings
+
+from pidservices.clients import PidmanRestClient
+
+class DjangoPidmanRestClient(PidmanRestClient):
+    """
+    Wraps :class:`PidmanRestClient` to use Pidman host and connection
+    information from the Django settings file instead of having to define it in
+    the class call.
+
+    NOTE:  The following values **MUST** be added to your projects django
+           settings.py file for this class to work properly.
+
+           * PIDMAN_HOST = '' # base url for the pidman server to query. i.e.
+             'http://pidman.library.emory.edu/' note the trailing shash.
+           * PIDMAN_USER = '' # Username for authentication to the pidman app.
+           * PIDMAN_PASSWORD = '' # Pasword for username above.
+           
+    """
+
+    def __init__(self):
+        try:
+            baseurl = settings.PIDMAN_HOST
+            username = settings.PIDMAN_USER
+            password = settings.PIDMAN_PASSWORD
+            super(DjangoPidmanRestClient, self).__init__(baseurl, username, password)
+        except AttributeError: # Raise error if values do not exist.
+            errmsg = """
+            Configuration Error!  The following values must be set in django 
+            settings.  PIDMAN_HOST, PIDMAN_USER, PIDMAN_PASSWORD
+
+            See pidmanclient documentation for more information.
+            """
+            raise RuntimeError(errmsg)
