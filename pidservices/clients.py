67e2bfed6f71752ac28e6028ab60a222a1a87044
Scott Turnbull <sturnbu@emory.edu>
2010-10-21 21:29:56 +0000

    Added Domain methods.

--- pidservices/clients.py
+++ pidservices/clients.py
@@ -9,7 +9,8 @@ TODO: Test this note out to see what it gets us.
 
 '''
 
-import urllib2, urllib, re
+import urllib, urllib2, httplib, base64, json
+from urlparse import urlparse
 
 class PidmanRestClient(object):
     """
@@ -18,19 +19,33 @@ class PidmanRestClient(object):
     build encapulated calls to the Pidman Rest API service.
 
     :param baseurl: base url of the api for the pidman REST service.
-                    note this requires a trailing slash.
+                    note this requires **NO** trailing slash. example
+                    'http://my.domain.com/pidserver'
     :param username: optional username to query REST API with.
-    :param password: optional password for username to query REST API
+    :param password: optional password for username to query REST API.  Stored
+                     with base64 encoding.
 
     """
+    baseurl = {
+        'scheme': None,
+        'host': None,
+        'path': None,
+    }
 
-    def __init__(self, baseurl, username="", password=""):
-        self.baseurl = self._set_baseurl(baseurl)
+    headers = {
+        "Content-type": "application/rest-urlencoded",
+        "Accept": "text/plain",
+        "Content-Length": "0",
+        "User-Agent": "x-www-form-urlencoded format",
+    }
+
+    def __init__(self, url, username="", password=""):
+        self._set_baseurl(url)
         self.username = username
-        self.password = password
-        self.opener = self._get_opener()
+        self._set_password(password)
+        self.connection = self._get_connection()
 
-    def _set_baseurl(self, baseurl):
+    def _set_baseurl(self, url):
         """
         Provides some cleanup for consistency on the input url.  If it has no
         trailing slash it adds one.
@@ -38,53 +53,180 @@ class PidmanRestClient(object):
         :param baseurl: string of the base url for the rest api to be normalized
 
         """
-        tail = re.compile('.*\/$')
-        if tail.match(baseurl) is None: # Add forward slash to url if not there.
-           baseurl = '/'.join([baseurl, ''])
-        return baseurl
+        obj = urlparse(url)
+        self.baseurl['scheme'] = obj.scheme
+        self.baseurl['host'] = obj.netloc
+        self.baseurl['path'] = obj.path
 
-    def _get_opener(self):
+    def _get_baseurl(self):
         """
-        Constructs the url opener for connection and authentication to the REST
-        API service
+        Returns the baseurl used.  Mostly for error checking.
+        """
+        return '%s://%s%s' % (self.baseurl['scheme'], self.baseurl['host'], self.baseurl['path'])
+
+    def _get_connection(self):
+        """
+        Constructs the proper httplib connection object based on the
+        baseurl.scheme value.
 
         """
-        passmgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
-        passmgr.add_password(None, self.baseurl, self.username, self.password)
-        handler = urllib2.HTTPBasicAuthHandler(passmgr)
-        return urllib2.build_opener(handler)
+        if self.baseurl['scheme'] is 'https':
+            return httplib.HTTPSConnection(self.baseurl['host'])
+        return httplib.HTTPConnection(self.baseurl['host'])
 
-    def request_pid(self, pid):
-        pass
+    def _secure_headers(self):
+        """Returns a copy of headers with the intent of using that as a
+        method variable so I'm not passing username and password by default.
+        It's private because... get your own darn secure heaeders ya hippie!
+        """
+        headers = self.headers.copy()
+        headers["username"] = self.username
+        headers["password"] = self.password
+        return headers
 
-    def request_domain(self, domain):
-        pass
+    def _set_password(self, password):
+        """Base 64 encodes the password."""
+        self.password = base64.b64encode(password)
 
-    def search_pids(self, querydict={}):
+    def list_domains(self):
+        """
+        Returns the default domain list from the rest server.
+        """
+        headers = self.headers
+        conn = self.connection
+        url = '%s/domains/' % self.baseurl['path']
+        conn.request("GET", url, None, headers)
+        response = conn.getresponse()
+        if response.status is not 200:
+            conn.close()
+            raise urllib2.HTTPError(url, response.status, response.reason, None, None)
+        else:
+            data = response.read()
+            conn.close()
+            return json.loads(data)
+        
+    def create_domain(self, name, policy=None, parent=None):
+        """
+        Creates a POST request to the rest api with attributes to create
+        a new domain.
+
+        :param name: label or title for the new  Domain
+        :param policy: policy title
+        :param parent: parent uri
+        
+        """
+        # Do some error checking before we bother sending the request.
+        if not name or name == '':
+            raise Exception('Name value cannot be None or empty!')
+
+        headers = self._secure_headers()
+
+        # Work the request.
+        domain = {'name': name, 'policy': policy, 'parent': parent}
+        params = urllib.urlencode(domain)
+        conn = self.connection
+        url = '%s/domains/' % self.baseurl['path']
+        conn.request("POST", url, params, headers)
+        response = conn.getresponse()
+        if response.status is not 201: # 201 is the expected return on create.
+            raise urlib2.HTTPError(url, response.status, response.reason, None, None)
+        else:
+            return response.read() # Should be a text response about success.
+
+    def request_domain(self, domain_id):
+        """
+        Requests a domain by id.
+
+        :param domain_id: ID of the domain to return.
+        
+        """
+        headers = self._secure_headers()
+        conn = self.connection
+        url = '%s/domains/%s/' % (self.baseurl['path'], urllib.quote(str(domain_id)))
+        conn.request("GET", url, None, headers)
+        response = conn.getresponse()
+        if response.status is not 200:
+           raise urlib2.HTTPError(url, response.status, response.reason, None, None)
+        else:
+            data = response.read() 
+            return json.loads(data)
+
+    def update_domain(self, id, name=None, policy=None, parent=None):
+        """
+        Updates an existing domain with new information.
+
+        :param name: label or title for the Domain
+        :param policy: policy title
+        :param parent: parent uri
+        
+        """
+        # Work a bit with the arguments to get them in a dict and filtered.
+        domain = {}
+        args = locals()
+        del args['self']
+        del args['id']
+        for key, value in args.items():
+            if value:
+                domain[key] = value
+        
+        # Setup the data to pass in the request.
+        headers = self._secure_headers()
+        url = '%s/domain/%s/' % (self.baseurl['path'], id)
+        body = '%s' % json.dumps(domain)
+
+        if not domain:
+            raise urllib2.HTTPError(url, 412, "No data provided for a valid updated", body, None)
+
+        conn = self.connection
+        conn.request("PUT", url, body, headers)
+        response = conn.getresponse()
+        if response.status is not 200:
+            raise urllib2.HTTPError(url, response.status, response.reason, None, None)
+        else:
+            # If successful the view returns the object just updated.
+            data = response.read()
+            return json.loads(data)
+
+    def delete_domain(self, domain):
+        """
+        You can't delete domains, don't even try.
+
+        :param domain: Any value of a domain, it doesn't matter.  I wont let you
+                       delete it anyway.
+        """
+        raise Exception("WHAT YOU TALKIN' 'BOUT WILLIS!?!?!  You can't delete domains.")
+
+    def search_pids(self, pid=None, type=None, target=None, domain=None, page=None, count=None):
         """
         Queries the PID search api and returns the data results.
 
-        :param querydict: dict of attribute value pairs for PID searches.
-                          Currently known valid values are:
-
-                           * domain - exact domain uri for pid
-                           * type - purl or ark
-                           * pid - exact pid value
-                           * target - exact target uri
-                           * page - Page number of results to return
-                           * count - Number of results to return on a single
-                             page
+        :param domain: Exact domain uri for pid
+        :param type: purl or ark
+        :param pid: Exact pid value
+        :param target: Exact target uri
+        :param page: Page number of results to return
+        :param count: Number of results to return on a single page.
 
         """
+        # If any of the arguments have been set, construct a querystring out of
+        # them.  Skip anything left null.
+        query = {}
+        args = locals()
+        del args['self']
+        for key, value in args.items():
+            if value:
+                query[key] = value
 
-        querystring = urllib.urlencode(querydict) # Encode the params
-        url = '%spids/' % self.baseurl # format the search url
-        urllib2.install_opener(self.opener) # install the opener
+        querystring = urllib.urlencode(query)
+        headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain", "Content-Length": "0"}
+        conn = self.connection
+        url = '%s/pids/?%s' % (self.baseurl['path'], querystring)
+        conn.request('GET', url, None, headers)
+        response = conn.getresponse()
+        if response.status is not 200:
+            raise urllib2.HTTPError(url, response.status, response.reason, None, None)
+        else:
+            data = response.read()
+            return data
 
-        # Try our actual query and catch normal errors.
-        try:
-            return urllib2.urlopen(url, querystring).read()
-        except urllib2.HTTPError as e:
-            err = "%s for %s?%s" % (e, url, querystring)
-            # print err
-            return err
+
--- tests.py
+++ tests.py
@@ -5,8 +5,7 @@
 
 """
 
-import os
-import unittest
+import os, unittest, base64, json, urllib2
 
 # from django.core.management import setup_environ
 from django.conf import settings
@@ -20,32 +19,157 @@ settings.configure(
 from pidservices.clients import PidmanRestClient
 from pidservices.djangowrapper.shortcuts import DjangoPidmanRestClient
 
+# Mock httplib so we don't need an actual server to test against.
+class MockHttpResponse():
+
+    def __init__(self):
+        self.status = 200
+        self.reason = "this is my reason"
+        self.data = None
+
+    def set_data(self, data):
+        """
+        Sets the data expected in the return.
+        """
+        self.data = data
+
+    def set_status(self, code):
+        self.status = code
+
+    def read(self):
+        """Returns the data as per a read."""
+        return self.data
+
+class MockHttpConnection():
+
+    def __init__(self):
+        self.response = MockHttpResponse()
+
+    def request(self, method, url, postvalues, headers):
+        self.method = method
+        self.url = url
+        self.postvalues = postvalues
+        self.headers = headers
+        
+    def getresponse(self):
+        return self.response
+
+    def close(self):
+        pass
+
+class MockHttplib:
+
+    def __init__(self, url):
+        self.url = url
+        self.connection = MockHttpConnection()
+
+# Test the normal pidman client.
+
 class PidmanRestClientTest(unittest.TestCase):
 
     def setUp(self):
-        baseurl = 'http://brutus.library.emory.edu/pidman/'
-        username = ''
-        password = ''
-        self.noauth_client = PidmanRestClient(baseurl, username, password)
+        self.baseurl = 'http://brutus.library.emory.edu/pidman'
+        self.username = 'testuser'
+        self.password = 'testuserpass'
+
+    def _new_client(self):
+        """
+        Returns a client with a mock connectin object for testing.
+        """
+        client = PidmanRestClient(self.baseurl, self.username, self.password)
+        mock = MockHttplib(client.baseurl['host'])
+        client.connection = mock.connection # Replace the normal connection for testing.
+        return client
+
+    def test_constructor(self):
+        """Tests the proper constructor values are set"""
+        client = self._new_client()
+        self.assertEqual(client.baseurl['scheme'],
+            'http', 'Scheme not set to http as spected for baseurl!')
+        self.assertEqual(client.baseurl['host'],
+            'brutus.library.emory.edu', 'Host not correctly set for baseurl!')
+        self.assertEqual(client.baseurl['path'],
+            '/pidman', 'Path not correctly set for baseurl!')
+        self.assertNotEqual(client.password, 'testuserpass',
+            'Password has not been encoded!')
 
     def test_search_pids(self):
         """Tests the REST return for searching pids."""
-
-        data = self.noauth_client.search_pids({})
-        # TODO: Change this to a real test when the staging server is udpated
-        # to include the rest API code.
+        # Be a normal return.
+        norm_client = self._new_client()
+        norm_client.connection.response.data = '[{"pid": "testblank"}]'
+        data = norm_client.search_pids({})
         self.assertTrue(data, "No return when trying to search pids!!")
 
+        # This shoule error
+        bad_client = self._new_client()
+        bad_client.connection.response.set_status(201)
+        self.assertRaises(urllib2.HTTPError, bad_client.search_pids)
+
+    def test_list_domains(self):
+        """Tests the REST list domain method."""
+        data_client = self._new_client()
+        data_client.connection.response.data = '[{"pid": "testblank"}]'
+        data = data_client.list_domains()
+        self.assertTrue(data, "No data returned when listing domains.")
+
+        # This shoule error
+        bad_client = self._new_client()
+        bad_client.connection.response.set_status(201)
+        self.assertRaises(urllib2.HTTPError, bad_client.search_pids)
+
+    def test_create_domain(self):
+        """Tests the creation of the domain."""
+        # Test a normal working return.
+        client = self._new_client()
+        client.connection.response.data = ''
+        client.connection.response.status = 201
+        client.create_domain('Test Domain')
+        # I'm actually just testing that this doesn't throw an error.
+        self.assertEqual(201, client.connection.response.status)
+
+        # This SHOULD thrown an error.
+        bad_client = self._new_client()
+        self.assertRaises(Exception, bad_client.create_domain, None)
+
+    def test_request_domain(self):
+        """Tests the request and return of a single domain."""
+        client = self._new_client()
+        client.connection.response.data = '[{"id": 25, "name": "domain name"}]'
+        domain = client.request_domain(25)
+        self.assertEqual(25, domain[0]['id'])
+
+    def test_update_domain(self):
+        """Tests the update method for a single domain."""
+        client = self._new_client()
+        client.connection.response.data = '[{"id": 25, "name": "The Updated Domain", "policy": "", "parent": ""}]'
+        domain = client.update_domain(25, name='The Updated Domain')
+
+        # Test a normal response to ensure it's giving back a pythonic object.
+        self.assertEqual(200, client.connection.response.status) # Check the Return
+        self.assertEqual('The Updated Domain', domain[0]['name'], "Domain not parsed as expected!")
+
+        # Make sure it throws an error if passed no Data.
+        client.connection.response.data = ''
+        self.assertRaises(urllib2.HTTPError, client.update_domain, 25)
+
+        # Make sure it returns other errors if returned by server.
+        client.connection.response.data = '[{"id": 25, "name": "The Updated Domain", "policy": "", "parent": ""}]'
+        client.connection.response.status = 500
+        self.assertRaises(urllib2.HTTPError, client.update_domain, 25, name="The Updated Domain")
+
+
+# Test the Django wrapper code for pidman Client.
 class DjangoPidmanRestClientTest(unittest.TestCase):
 
      def test_constructor(self):
         client = DjangoPidmanRestClient()
-        self.assertEqual(client.baseurl,
-            'http://testpidman.library.emory.edu/',
+        self.assertEqual(client.baseurl['host'],
+            'testpidman.library.emory.edu',
             'Client Base URL %s not expected value.' % client.baseurl)
         self.assertEqual(client.username, 'testuser',
             'Client username %s not the expected value' % client.username)
-        self.assertEqual(client.password, 'testpass',
+        self.assertEqual(client.password, base64.b64encode('testpass'),
             'Client password %s is not expected value' % client.password)
 
      def test_runtime_error(self):
@@ -55,6 +179,11 @@ class DjangoPidmanRestClientTest(unittest.TestCase):
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(PidmanRestClientTest("test_search_pids"))
+    suite.addTest(PidmanRestClientTest("test_constructor"))
+    suite.addTest(PidmanRestClientTest("test_list_domains"))
+    suite.addTest(PidmanRestClientTest("test_create_domain"))
+    suite.addTest(PidmanRestClientTest("test_request_domain"))
+    suite.addTest(PidmanRestClientTest("test_update_domain"))
     suite.addTest(DjangoPidmanRestClientTest("test_constructor"))
     suite.addTest(DjangoPidmanRestClientTest("test_runtime_error"))
     return suite
